e62fb0bac4e77542bde32c55b0c95eb429f7b247
RylinWolf <1971257026@qq.com>
2026-03-24 15:41:18 +0800

    fix: 修复时间字段处理问题；
    - 修复MySQL 至 Influx 数据转换器中，时间字段不携带时区时转换失败的问题

--- src/main/java/com/wolfhouse/dbmig/core/datasource/adaptor/converter/MySql2InfluxConverter.java
+++ src/main/java/com/wolfhouse/dbmig/core/datasource/adaptor/converter/MySql2InfluxConverter.java
@@ -5,6 +5,8 @@ import com.wolfhouse.dbmig.core.datasource.sourcedata.MySqlData;
 import com.wolfhouse.dbmig.properties.MigrateProperty;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.time.temporal.Temporal;
 import java.util.Map;
 
@@ -17,13 +19,35 @@ public class MySql2InfluxConverter implements SubConverter<MySqlData, InfluxData
     @Override
     public InfluxData convert(MySqlData source, MigrateProperty.Field fieldConf) {
         Map<String, Object> map = source.toMap();
-        map.forEach((k, v) -> {
+        map.replaceAll((k, v) -> {
             // 特殊处理时间字段
-            if (Temporal.class.isAssignableFrom(v.getClass())) {
-                map.put(k, Instant.from((Temporal) v));
-            }
+            Object instant = processInstant(v);
+            return instant != null ? instant : v;
         });
         return InfluxData.of(map);
+    }
+
+    /**
+     * 处理时间字段。
+     * 字段若为时间，则尝试将其封装为 Instant，否则返回 null。
+     * 无法封装为 Instant，则返回字符串表示。
+     *
+     * @param v 要处理的数据
+     * @return 若数据可封装为 Instant，则返回为 Instant。若数据为时间，则返回字符串，否则返回 null。
+     */
+    private Object processInstant(Object v) {
+        Class<?> vClass = v.getClass();
+        if (!Temporal.class.isAssignableFrom(vClass)) {
+            return null;
+        }
+        if (Instant.class.isAssignableFrom(vClass)) {
+            return Instant.from((Temporal) v);
+        }
+        if (LocalDateTime.class.isAssignableFrom(vClass)) {
+            LocalDateTime vDateTime = (LocalDateTime) v;
+            return vDateTime.atZone(ZoneOffset.UTC).toInstant();
+        }
+        return v.toString();
     }
 
     @Override
