6a4e8e6808cfb88173b69a6a7544abaf57672c3a
RylinWolf <1971257026@qq.com>
2026-03-25 10:09:23 +0800

    feat: 完善 MySql 适配器，实现适配方法

--- src/main/java/com/wolfhouse/dbmig/core/datasource/adaptor/MySqlAdaptor.java
+++ src/main/java/com/wolfhouse/dbmig/core/datasource/adaptor/MySqlAdaptor.java
@@ -1,25 +1,37 @@
 package com.wolfhouse.dbmig.core.datasource.adaptor;
 
+import com.wolfhouse.dbmig.core.datasource.adaptor.converter.ConverterFactory;
+import com.wolfhouse.dbmig.core.datasource.adaptor.converter.SubConverter;
 import com.wolfhouse.dbmig.core.datasource.sourcedata.BaseSourceData;
+import com.wolfhouse.dbmig.core.datasource.sourcedata.InfluxData;
 import com.wolfhouse.dbmig.core.datasource.sourcedata.MySqlData;
 import com.wolfhouse.dbmig.properties.MigrateProperty;
 
 import java.util.Collection;
+import java.util.Set;
 
 /**
  * MySQL 数据适配器
  *
  * @author Rylin Wolf
  */
+@SuppressWarnings({"unchecked"})
 public class MySqlAdaptor extends BaseDataAdaptor<MySqlData> {
+    private static final Set<Class<? extends BaseSourceData>> SUPPORTED_FROM;
+
+    static {
+        SUPPORTED_FROM = Set.of(InfluxData.class);
+    }
+
     @Override
     public boolean fromSupport(Class<? extends BaseSourceData> clazz) {
-        return false;
+        return SUPPORTED_FROM.contains(clazz);
     }
 
     @Override
     public <T extends BaseSourceData> MySqlData adaptFrom(T obj) {
-        return null;
+        SubConverter<T, MySqlData> converter = (SubConverter<T, MySqlData>) ConverterFactory.getConverter(MySqlData.class, obj.getClass());
+        return converter.convert(obj, null);
     }
 
     @Override
